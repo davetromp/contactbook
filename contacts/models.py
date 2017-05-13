5d778455d9d27cfda0230962ba3e64eeeaf2f0fc
davetromp <david.tromp@maakjehuishoom.nl>
2017-05-13 17:35:55 +0200

    redo the initial migration toreflect the complete model with thumbnail

--- contacts/models.py
+++ contacts/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 
 class Contact(models.Model):
-    firstname = models.CharField(db_index=True, max_length=255)
-    lastname = models.CharField(db_index=True, max_length=255)
-    street = models.CharField(db_index=True, max_length=255)
-    zipcode = models.CharField(db_index=True, max_length=255)
-    city = models.CharField(db_index=True, max_length=255)
-    image_url = models.CharField(db_index=True, max_length=255)
-    thumbnail = models.TextField()
+    firstname = models.CharField(default=None, db_index=True, blank=True, null=True, max_length=255)
+    lastname = models.CharField(default=None, db_index=True, blank=True, null=True, max_length=255)
+    street = models.CharField(default=None, db_index=True, blank=True, null=True, max_length=255)
+    zipcode = models.CharField(default=None, db_index=True, blank=True, null=True, max_length=255)
+    city = models.CharField(default=None, db_index=True, blank=True, null=True, max_length=255)
+    image_url = models.CharField(default=None, db_index=True, blank=True, null=True, max_length=255)
+    thumbnail = models.TextField(default=None, db_index=True, blank=True, null=True)
 
     def __unicode__(self):
         return self.firstname + " " + self.lastname
